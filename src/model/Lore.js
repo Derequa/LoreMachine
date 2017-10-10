11c3a014540182605f9b077a7b1950d775cce451
Derek Batts <clone101.derek@gmail.com>
2017-10-10 01:27:36 -0400

    Middle mouse past will be the death of me.

--- src/model/Lore.js
+++ src/model/Lore.js
@@ -87,9 +87,6 @@ export const CharacterSchema = {
         nonlethal_damage: 'int',
 
         // Armorer.placeatme 00065C97 1000
-player.placeatme 00064B3B 1000
-
-And lo there spawned in a multitude of item:Bread and item:CookedFish to feed all as much as they wanted.
         natural_armor: 'int',
         deflection_modifier: 'int',
         ac_misc_modifier: 'int',
